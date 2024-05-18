2f8118b24d97c328a41d10f8b87a8aa013d9d9db
Juan René Soler SirÍ <juanr.soler_93@hotmail.es>
2024-05-18 12:51:18 -0400

    implemente extention method para concatenar un or a las expresion lambda en los query de los filtros.

--- src/back/dotnet/Infrastructure/Extentions/ExtentionsExpressions.cs
+++ src/back/dotnet/Infrastructure/Extentions/ExtentionsExpressions.cs
@@ -11,4 +11,11 @@ public static class ExtentionsExpressions
         var body = Expression.AndAlso(Expression.Invoke(expression, parameter), Expression.Invoke(andExpression, parameter));
         return Expression.Lambda<Func<T, bool>>(body, parameter);
     }
+
+    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expression, Expression<Func<T, bool>> orExpression)
+    {
+        var parameter = Expression.Parameter(typeof(T), "x");
+        var body = Expression.OrElse(Expression.Invoke(expression, parameter), Expression.Invoke(orExpression, parameter));
+        return Expression.Lambda<Func<T, bool>>(body, parameter);
+    }
 }
